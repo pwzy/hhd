58a70dc23fe47b2674abec424c95a762b383aa28
pwzy <2386900869@qq.com>
2020-10-21 10:09:10 +0800

    new

--- features.py
+++ features.py
@@ -13,24 +13,27 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     data_set = Data_Set('../data/ped2/training/frames', resize_height=360, resize_width=360, time_steps=5)
-    train_loader = DataLoader(data_set, batch_size=2, shuffle=True, num_workers=32)
+    batch_size = 2
+    train_loader = DataLoader(data_set, batch_size=batch_size, shuffle=True, num_workers=32)
 
 
-    iterator = iter(train_loader)
-    # x is a list which contains [I0, I1, I2, I3, I4]
-    x = next(iterator)
-    print(len(x))
-
-    # import resnet50 model
-    resnet50 = models.resnet50(pretrained=True, progress=True)
-    resnet50.eval()
+    #  iterator = iter(train_loader)
+    #  #  x is a list which contains [I0, I1, I2, I3, I4]
+    #  x = next(iterator)
+    #  # stack the different image
+    #  x = torch.cat([x[i] for i in range(5)], dim=0)
+    #  print(len(x))
+    #
+    #  # import resnet50 model
+    #  resnet50 = models.resnet50(pretrained=True, progress=True)
+    #  resnet50.eval()
+    #  image_feature = resnet50(x) # this size is [2,1000]
+    #  print(x.shape)
+    #  print(image_feature.shape)
     
-    
-    
-    image_feature = resnet50(x[0]) # this size is [2,1000]
 
     print(device)
-    print('test')
+    print('the end ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
     # prepare net 
     # TODO
